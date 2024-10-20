b14302504b1fa01c5d8dec1fffb4161ce28d739d
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-20 23:09:59 +0530

    main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ import logging
 #     api_hash=api_hash,
 #     bot_token=bot_token)
 
-@bot.on_message(filters.command(["start"]))
-async def start( Client, m: Message):
+@bot.on_message(filters.command(["aditya"]))
+async def start(bot: Client, m: Message):
     await m.reply_text(f"<b>Hello {m.from_user.mention} 👋\n\n I Am A Bot For Download Links From Your **.TXT** File And Then Upload That File On Telegram So Basically If You Want To Use Me First Send Me /run Command And Then Follow Few Steps..\n Extracted By ➤ 🅹🅰🅸 🆂🅷🆁🅸 🆁🅰🅼 \n\nUse /stop to stop any ongoing task.</b>")
 
 
